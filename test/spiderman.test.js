d5afe2c2b51c95a09fca4a4fa28714e42b850783
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 16:05:34 -0500

    Agregando estructura para proyecto con una prueba unitaria

---
+++ test/spiderman.test.js
@@ -0,0 +1,9 @@
+const { TestWatcher } = require("jest")
+
+describe('Prueba Unitaria para agregar datos para un spiderman a la clase spiderman', () =>{
+    Test('Prueba 1 Spiderman', () => {
+
+        const result = 1+2
+        expect(result).toBe(18)
+    })
+})
