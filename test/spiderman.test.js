62d4ab07a90714d838a3d8033a02eca194eb2b08
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 16:17:03 -0500

    2-Agregando archivo js a app para ir haciendo funcionar la prueba

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,9 +1,12 @@
-const { TestWatcher } = require("jest")
-
 describe('Prueba Unitaria para agregar datos para un spiderman a la clase spiderman', () =>{
-    Test('Prueba 1 Spiderman', () => {
-
-        const result = 1+2
-        expect(result).toBe(18)
+    Test('Prueba 1 Spiderman objeto', () => {
+        // Codogigo que vamos a utlizar tal cual
+        const AndrewGarfield = new Spiderman('Spiderman Sony',31,'Angrew GArgield',2,'Sony')
+        // Validamos el codigo que esperamos
+        expect(AndrewGarfield.name).toBe('Spiderman Sony')
+        expect(AndrewGarfield.name).toBe(31)
+        expect(AndrewGarfield.name).toBe('Andrew Garfield')
+        expect(AndrewGarfield.name).toBe(31)
+        expect(AndrewGarfield.name).toBe('Sony')
     })
 })
