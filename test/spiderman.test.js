98ee455a9278f7c8c5af2effe89c96e56443ad6e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 16:35:39 -0500

    -5 Terminado los requerimientos prueba 1 funcionando

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -3,12 +3,12 @@ const Spiderman = require('./../app/spiderman')
 describe('Prueba Unitaria para agregar datos para un spiderman a la clase spiderman', () =>{
     test('Prueba 1 Spiderman objeto', () => {
         // Codogigo que vamos a utlizar tal cual
-        const AndrewGarfield = new Spiderman('Spiderman Sony',31,'Angrew GArgield',2,'Sony')
+        const AndrewGarfield = new Spiderman('Spiderman Sony',31,'Andrew Garfield',2,'Sony')
         // Validamos el codigo que esperamos
         expect(AndrewGarfield.name).toBe('Spiderman Sony')
-        expect(AndrewGarfield.name).toBe(31)
-        expect(AndrewGarfield.name).toBe('Andrew Garfield')
-        expect(AndrewGarfield.name).toBe(31)
-        expect(AndrewGarfield.name).toBe('Sony')
+        expect(AndrewGarfield.age).toBe(31)
+        expect(AndrewGarfield.actor).toBe('Andrew Garfield')
+        expect(AndrewGarfield.movies).toBe(2)
+        expect(AndrewGarfield.studio).toBe('Sony')
     })
 })
