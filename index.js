c3956b6206d64c44e379872e56a2e5d4f7427211
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-24 16:48:41 -0500

    7- Agregando datos de pruebas unitarias al index

---
+++ index.js
@@ -0,0 +1,7 @@
+const Spiderman = require('./app/spiderman')
+
+
+const AndrewGarfield = new Spiderman('Spiderman Sony',31,'Andrew Garfield',2,'Sony')
+const tomHolland = new Spiderman('Spiderman Marvel',25,'Tom Holland',5,'Marvel')
+console.log(AndrewGarfield)
+console.log(tomHolland)
